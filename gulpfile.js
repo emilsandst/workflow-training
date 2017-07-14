87b54cb282b2202668af549cb03e9c568d420d7f
Emil <emilsandst@gmail.com>
2017-07-14 17:10:07 +0200

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
-
 var sassSources = ['components/sass/style.scss']
 
 gulp.task('coffee', function() {
@@ -38,6 +37,12 @@ gulp.task('compass', function() {
     })
       .on('error', gutil.log))
     .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
 });
 
 gulp.task('default', ['coffee', 'js', 'compass']);
